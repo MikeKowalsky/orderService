fb2159765f2e3ba7ccbb6ef38a52d7bff3a14749
Michal Kowalewski <mkowalewski83@gmail.com>
2019-09-03 23:28:50 +0200

    store state transitions

--- src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -14,6 +14,8 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.access.StateMachineAccess;
+import org.springframework.statemachine.access.StateMachineFunction;
 import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
@@ -23,6 +25,9 @@ import org.springframework.statemachine.config.builders.StateMachineStateConfigu
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.statemachine.support.StateMachineInterceptorAdapter;
+import org.springframework.statemachine.transition.Transition;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +35,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 public class OrderServiceApplication {
@@ -69,6 +76,14 @@ class Runner implements ApplicationRunner {
 
 		Order order = this.orderService.create(new Date());
 
+		StateMachine<OrderStates, OrderEvents> paymentStateMachine = orderService.pay(order.getId(), UUID.randomUUID().toString());
+		log.info("after calling pay() " + paymentStateMachine.getState().getId().name());
+		log.info("order " + orderService.byId(order.getId()));
+
+		StateMachine<OrderStates, OrderEvents> fulfillmentStateMachine = orderService.fulfill(order.getId());
+		log.info("after calling fulfill() " + fulfillmentStateMachine.getState().getId().name());
+		log.info("order " + orderService.byId(order.getId()));
+
 	}
 }
 
@@ -76,7 +91,7 @@ class Runner implements ApplicationRunner {
 interface OrderRepository extends JpaRepository<Order, Long>{}
 
 
-@Entity
+@Entity (name = "ORDERS")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -106,15 +121,84 @@ class Order {
 class OrderService {
 
 	private final OrderRepository orderRepository;
+	private final StateMachineFactory<OrderStates, OrderEvents> factory;
+	private static final String ORDER_ID_HEADER = "orderId";
 
-	OrderService(OrderRepository orderRepository){
+	OrderService(OrderRepository orderRepository, StateMachineFactory<OrderStates, OrderEvents> factory){
 		this.orderRepository = orderRepository;
+		this.factory = factory;
 	}
 
-	Order create(Date when){
-		return this.orderRepository
-				.save(new Order(when, OrderStates.SUBMITTED));
+	Order byId (Long id){
+		return this.orderRepository.findById(id).get();
 	}
+
+	Order create (Date when){
+		return this.orderRepository.save(new Order(when, OrderStates.SUBMITTED));
+	}
+
+	StateMachine<OrderStates, OrderEvents> pay (Long orderId, String paymentConfirmationNumber){
+		StateMachine<OrderStates, OrderEvents> sm = this.build(orderId);
+
+		Message<OrderEvents> paymentMessage = MessageBuilder.withPayload(OrderEvents.PAY)
+				.setHeader(ORDER_ID_HEADER, orderId)
+				.setHeader("paymentConfirmationNumber", paymentConfirmationNumber)
+				.build();
+
+		sm.sendEvent(paymentMessage);
+		return sm;
+	}
+
+	StateMachine<OrderStates, OrderEvents> fulfill (Long orderId){
+		StateMachine<OrderStates, OrderEvents> sm = this.build(orderId);
+
+		Message<OrderEvents> fulfillmentMessage = MessageBuilder.withPayload(OrderEvents.FULFILL)
+				.setHeader(ORDER_ID_HEADER, orderId)
+				.build();
+
+		sm.sendEvent(fulfillmentMessage);
+		return sm;
+	}
+
+
+	private StateMachine<OrderStates, OrderEvents> build (Long orderId) {
+		Order order = this.orderRepository.findById(orderId).get();
+		String orderIdKey = Long.toString(order.getId());
+
+		StateMachine<OrderStates, OrderEvents> sm = this.factory.getStateMachine(orderIdKey);
+
+		sm.stop();
+
+		sm.getStateMachineAccessor()
+			.doWithAllRegions(sma -> {
+
+				sma.addStateMachineInterceptor(new StateMachineInterceptorAdapter<OrderStates, OrderEvents>(){
+					@Override
+					public void preStateChange(State<OrderStates, OrderEvents> state, Message<OrderEvents> message, Transition<OrderStates, OrderEvents> transition, StateMachine<OrderStates, OrderEvents> stateMachine) {
+
+						Optional.ofNullable(message).ifPresent(msg -> {
+
+							Optional.ofNullable(Long.class.cast(msg.getHeaders().getOrDefault(ORDER_ID_HEADER, -1L)))
+								.ifPresent(orderId -> {
+									Order order = orderRepository.findById(orderId).get();
+									order.setOrderState(state.getId());
+									orderRepository.save(order);
+								});
+
+						});
+					}
+				});
+
+				sma.resetStateMachine(new DefaultStateMachineContext<>(order.getOrderState(), null, null, null));
+
+			});
+
+		sm.start();
+
+		return sm;
+	}
+
+
 }
 
 
@@ -158,7 +242,7 @@ class SimpleEnumStatemachineConfiguration extends StateMachineConfigurerAdapter 
 		StateMachineListenerAdapter<OrderStates, OrderEvents> adapter = new StateMachineListenerAdapter<OrderStates, OrderEvents>(){
 			@Override
 			public void stateChanged(State<OrderStates, OrderEvents> from, State<OrderStates, OrderEvents> to) {
-				log.info(String.format("stateChanged(from: %s, to: %s)", from + "", to + ""));
+			log.info(String.format("stateChanged(from: %s, to: %s)", from + "", to + ""));
 			}
 		};
 		config.withConfiguration()
